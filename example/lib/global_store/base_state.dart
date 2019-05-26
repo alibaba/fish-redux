db7ef85316bfa54629426b73c6b297f721a76992
zjuwjf <>
2019-05-26 22:09:37 +0800

    Modify the use of global state in example

--- example/lib/global_store/base_state.dart
+++
@@ -1,7 +0,0 @@
-import 'package:fish_redux/fish_redux.dart';
-import 'state.dart';
-
-/// Definition of Cloneable
-mixin GlobalBaseState<T extends Cloneable<T>> implements Cloneable<T> {
-  T lessClone(GlobalState state);
-}
