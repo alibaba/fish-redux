9910f92b8c536dfbaf15ec6460e27e3fe8e4151c
m <vivask770@163.com>
2019-09-08 11:52:56 +0800

    1.add custom compare feature 2.finer control

--- lib/src/redux_component_mixin/private_reducer_mixin.dart
+++ lib/src/redux_component_mixin/private_reducer_mixin.dart
@@ -1,3 +1,5 @@
+import 'package:meta/meta.dart';
+
 import '../redux/redux.dart';
 import '../redux_component/redux_component.dart';
 
@@ -7,16 +9,52 @@ import '../redux_component/redux_component.dart';
 ///     ///
 ///   );
 /// }
+///
+/// custom usage
+/// class MyComponent extends Component<T> with PrivateReducerMixin<T> {
+///   MyComponent():super(
+///     ///
+///   );
+///
+///   //-->if true: Automatically convert Action to PrivateAction
+///   //-->if false: You must manually create a PrivateAction
+///   //default value -->  true
+///   //see [PrivateReducerMixin.wantAutoConvert]
+///   @override
+///   bool get wantAutoConvert => true;
+///
+///
+///   //default value --> state==other
+///   //see [PrivateReducerMixin.compare]
+///   @override
+///   bool compare(PrivateTestState state, dynamic other) {
+///     if (other is PrivateTestState) {
+///       return state.XxxBusinessId == other.XxxBusinessId;
+///     } else {
+///       return super.compare(state, other);
+///     }
+///   }
+///
+/// }
 mixin PrivateReducerMixin<T> on Logic<T> {
+  @protected
+  bool get wantAutoConvert => true;
+
+  @protected
+  bool compare(T state, dynamic other) => state == other;
+
   @override
   Reducer<T> get protectedReducer {
     final Reducer<T> superReducer = super.protectedReducer;
     return superReducer != null
         ? (T state, Action action) {
-            if (action is PrivateAction && action.target == state) {
-              return superReducer(state, action.asAction());
+            if (action is PrivateAction) {
+              return compare(state, action.target)
+                  ? superReducer(state, action.asAction())
+                  : state;
+            } else {
+              return superReducer(state, action);
             }
-            return state;
           }
         : null;
   }
@@ -25,20 +63,20 @@ mixin PrivateReducerMixin<T> on Logic<T> {
   Dispatch createDispatch(Dispatch effect, Dispatch next, Context<T> ctx) {
     final Dispatch superDispatch = super.createDispatch(effect, next, ctx);
     return (Action action) {
-      if (action.type is! Lifecycle && action is! PrivateAction) {
-        action = PrivateAction(
-          action.type,
-          payload: action.payload,
-          target: ctx.state,
-        );
+      if (action is PrivateAction) {
+        action.target ??= ctx.state;
+      } else if (wantAutoConvert && action.type is! Lifecycle) {
+        action = PrivateAction(action.type,
+            payload: action.payload, target: ctx.state);
       }
-      return superDispatch(action);
+      superDispatch(action);
     };
   }
 }
 
 class PrivateAction extends Action {
-  final Object target;
+  Object target;
+
   PrivateAction(Object type, {dynamic payload, this.target})
       : super(type, payload: payload);
 
